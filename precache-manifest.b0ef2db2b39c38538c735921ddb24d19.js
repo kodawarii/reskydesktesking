0c060219667a8f74e4fe3a6e8c16849fac0c34a2
eunbalman <bama_boy2@hotmail.com>
2019-02-04 16:44:05 +1300

    Updates

--- precache-manifest.b0ef2db2b39c38538c735921ddb24d19.js
+++ precache-manifest.b0ef2db2b39c38538c735921ddb24d19.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/reskydesktesking/static/js/runtime~main.461e50c8.js"
   },
   {
-    "revision": "5944f63d29ad7361c834",
-    "url": "/reskydesktesking/static/js/main.5944f63d.chunk.js"
+    "revision": "d622281a265e69a0c66e",
+    "url": "/reskydesktesking/static/js/main.d622281a.chunk.js"
   },
   {
     "revision": "f739feb7d61c48a46bed",
     "url": "/reskydesktesking/static/js/1.f739feb7.chunk.js"
   },
   {
-    "revision": "5944f63d29ad7361c834",
+    "revision": "d622281a265e69a0c66e",
     "url": "/reskydesktesking/static/css/main.17198d3b.chunk.css"
   },
   {
-    "revision": "c24bcf1478f7c5cbaaef8ea70676f21f",
+    "revision": "f05c045e30f93ccb42585088baff29ec",
     "url": "/reskydesktesking/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reskydesktesking/precache-manifest.a691cb6137362608e5de75473551a880.js"
+  "/reskydesktesking/precache-manifest.b0ef2db2b39c38538c735921ddb24d19.js"
 );
 
 workbox.clientsClaim();
