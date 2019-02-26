19c02151e606f68a5810bedfe288fbc664effab3
eunbalman <bama_boy2@hotmail.com>
2019-02-27 11:04:29 +1300

    Modified the Hole-Number-nav such that now 18 holes whos in a nice clean 2 lines

--- src/Components/HoleBanner.js
+++ src/Components/HoleBanner.js
@@ -10,9 +10,24 @@ class HoleBanner extends Component{
     }
 
     render(){
+        let numberOfHoles = this.props.numberOfHoles;
+
         let bufferArray = [];
-        for(var i = 0; i < this.props.numberOfHoles; i++){
-            bufferArray.push(i + 1);
+        let bufferArray2 = [];
+
+        if(numberOfHoles == 9){
+            for(var i = 0; i < 9; i++){
+                bufferArray.push(i + 1);
+            }
+        }
+        else{
+            for(var i = 0; i < 9; i++){
+                bufferArray.push(i + 1);
+            }
+
+            for(var i = 9; i < 18; i++){
+                bufferArray2.push(i + 1);
+            }
         }
 
         let collection = bufferArray.map(x => {
@@ -31,9 +46,31 @@ class HoleBanner extends Component{
             );
         });
 
+
+        let collection2;
+        if(numberOfHoles == 18){
+            collection2 = bufferArray2.map(x => {
+                let className = "holeBannerNumberCell holeNumber" + x;
+                
+                if(this.props.currentHole + 1 === parseInt(x)){
+                    className += " greyOutThisHole";
+                }
+    
+                if(this.props.holesData[parseInt(x) - 1].status){
+                    className += " finnishedHole";
+                }
+    
+                return(
+                    <span className={className} onClick={this.goToHole.bind(this, x)}> {x} </span>
+                );
+            });
+        }
+
         return(
             <div className="holeBannerContainer">
                 {collection} 
+                <br/>
+                {collection2}
             </div>
         );
     }
